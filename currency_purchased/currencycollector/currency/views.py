bf8489c311e1e41a07349ee211a8c4699c7d8029
Vishal <vishal.ranjan7777@gmail.com>
2025-06-29 19:00:10 -0500

    Get values from database and show it on the html file

--- currency_purchased/currencycollector/currency/views.py
+++ currency_purchased/currencycollector/currency/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import FractionalCurrencyVariety
 
 # Create your views here.
 def all_currency(request):
-    return render(request, 'currency/all_currency.html')
+    fractionCurrencies = FractionalCurrencyVariety.objects.all()
+    return render(request, 'currency/all_currency.html', { 'fractionals': fractionCurrencies })
