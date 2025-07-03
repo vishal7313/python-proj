7f99c00090040de91e66a892181fd7e5f4b1cac1
Vishal <vishal.ranjan7777@gmail.com>
2025-07-03 00:38:23 -0500

    Change to fractional

--- currency_purchased/currencycollector/currency/views.py
+++ currency_purchased/currencycollector/currency/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import FractionalCurrencyVariety
 
 # Create your views here.
-def all_currency(request):
+def fractional_currency(request):
     fractionCurrencies = FractionalCurrencyVariety.objects.all()
-    return render(request, 'currency/all_currency.html', { 'fractionals': fractionCurrencies })
+    return render(request, 'currency/fractional_currency.html', { 'fractionals': fractionCurrencies })
+
+def fractional_currency_details(request, fractional_currency_id):
+    currency = get_object_or_404(FractionalCurrencyVariety, pk=fractional_currency_id)
+    return render(request, 'currency/fractional_currency_details.html', { 'fractional': currency })
