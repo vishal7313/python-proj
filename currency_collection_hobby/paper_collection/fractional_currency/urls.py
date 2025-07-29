f48049d5b701d4c9d544e5f8e057cf7e0066c4a5
Vishal <vishal.ranjan7777@gmail.com>
2025-07-28 23:36:54 -0500

    Complete adding fractional app

--- currency_collection_hobby/paper_collection/fractional_currency/urls.py
+++ currency_collection_hobby/paper_collection/fractional_currency/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_fractional, name='all_fractional'),
+    path('<int:fractional_id>/', views.fractional_details, name='fractional_detail'),
 ]
--- currency_collection_hobby/paper_collection/fractional_currency/views.py
+++ currency_collection_hobby/paper_collection/fractional_currency/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import fractionalCurrencyVariety
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 # Create your views here.
 def all_fractional(request):
-    return render(request, 'fractional_currency/all_fractional.html')
+    fractionals = fractionalCurrencyVariety.objects.all()
+    return render(request, 'fractional_currency/all_fractional.html', { 'fractionals': fractionals })
+
+def fractional_details(request, fractional_id):
+    fractional = get_object_or_404(fractionalCurrencyVariety, pk=fractional_id)
+    return render(request, 'fractional_currency/fractional_currency_detail.html', { 'fractional': fractional })
