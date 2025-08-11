dd5d23366235ecef8ffc0c19fda87e48af8bdb8b
Vishal <vishal.ranjan7777@gmail.com>
2025-08-10 20:46:21 -0500

    Started woking on large size notes

--- currency_collection_hobby/paper_collection/paper_collection/settings.py
+++ currency_collection_hobby/paper_collection/paper_collection/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fractional_currency',
+    'large_size_notes',
     'tailwind',
     'theme',
     'django_browser_reload',
