25cc280d65f4b66d0fc318801ba1883cb1ff8745
Vishal <vishal.ranjan7777@gmail.com>
2025-08-11 23:00:50 -0500

    Add url and view and model for large size app

--- currency_collection_hobby/paper_collection/large_size_notes/views.py
+++ currency_collection_hobby/paper_collection/large_size_notes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import largeSizeCurrencyVariety
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 # Create your views here.
+def all_largesize_notes(request):
+    largenotes = largeSizeCurrencyVariety.objects.all()
+    return render(request, 'large_size_notes/all_large_size.html', { 'largenotes': largenotes })
+
+def largesize_details(request, largesize_id):
+    largenote = get_object_or_404(largeSizeCurrencyVariety, pk=largesize_id)
+    return render(request, 'large_size_notes/largesize_note_detail.html', { 'largesizenote': largenote })
--- currency_collection_hobby/paper_collection/paper_collection/urls.py
+++ currency_collection_hobby/paper_collection/paper_collection/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('fractional_currency/', include('fractional_currency.urls')),
-    
+    path('large_size_notes/', include('large_size_notes.urls')),
+
     
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
